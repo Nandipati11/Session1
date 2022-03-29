4c5291eaa7ad5e6d54d9d7e82a581a8faa9bdfb7
anilm <sunithauday2000@gmail.com>
2022-03-29 13:18:30 +0530

    added session1 tasks

---
+++ src/session1/email.java
@@ -0,0 +1,33 @@
+package session1;
+
+import java.util.ArrayList;
+
+public class email {
+	public static void main(String[] args) {
+		ArrayList<String> emailID = new ArrayList<String>();
+
+		emailID.add("sunitha@gmail.com");
+		emailID.add("pranitha@gmail.com");
+		emailID.add("anitha@gmail.com");
+		emailID.add("mahesh@gmail.com");
+		emailID.add("madhuri@gmail.com");
+		emailID.add("mahi@gmail.com");
+		emailID.add("iswarya@gmail.com");
+
+		String searcElement = "sunitha@gmail.com";
+
+		for (int i = 0; i < emailID.size(); i++) {
+
+			if (searcElement == emailID.get(i)) {
+
+				System.out.println("\n");
+
+				System.out.println("email ID " + searcElement + " found");
+
+				break;
+
+			}
+		}
+
+	}
+}
